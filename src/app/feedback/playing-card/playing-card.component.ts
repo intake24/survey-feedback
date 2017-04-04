02a703674ac412530b2eab42ae79dbed293efb8b
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-04-04 16:43:56 +0300

    brief details in cards

--- src/app/feedback/playing-card/playing-card.component.ts
+++ src/app/feedback/playing-card/playing-card.component.ts
@@ -7,6 +7,7 @@ import {
   CharacterSentimentWithDescription
 } from "../../classes/character.class";
 import {NutrientTypesService} from "../../services/nutrient-types.service";
+import {DemographicRange, DemographicScaleSectorSentimentEnum} from "../../classes/demographic-group.class";
 
 const CARD_SCENE_CLASS_BASE = "feedback-playing-card-";
 
@@ -29,14 +30,16 @@ export class PlayingCardComponent implements OnInit {
   height: number;
 
   sceneClass: string;
-  state: string;
 
   isVisible: boolean;
+
+  sentimentEnums: Object;
 
   @Input() characterDescription: CharacterSentimentWithDescription;
 
   constructor(private nutrientService: NutrientTypesService) {
     this.isVisible = false;
+    this.sentimentEnums = DemographicScaleSectorSentimentEnum;
   }
 
   ngOnInit(): void {
@@ -48,8 +51,11 @@ export class PlayingCardComponent implements OnInit {
       .map(dr => {
         let title = dr.resultedDemographicGroup.scaleSectors[0].name;
         let consumption = dr.consumption;
+        let sentiment = dr.resultedDemographicGroup.scaleSectors[0].sentiment;
+        let targetConsumption = dr.targetDemographicGroup.scaleSectors[0].range;
         let nutrientId = dr.resultedDemographicGroup.nutrientTypeId;
-        return new Subtitle(title, consumption, this.nutrientService.getUnitByNutrientTypeId(nutrientId).get);
+        return new Subtitle(title, consumption, targetConsumption,
+          this.nutrientService.getUnitByNutrientTypeId(nutrientId).get, sentiment);
       });
   }
 
@@ -63,11 +69,50 @@ class Subtitle {
 
   readonly title: string;
   readonly consumption: number;
+  readonly targetConsumption: DemographicRange;
   readonly units: string;
+  readonly sentiment: DemographicScaleSectorSentimentEnum;
+  readonly textClass: string;
+  readonly iconClass: string;
 
-  constructor(title: string, consumption: number, units: string) {
+  constructor(title: string, consumption: number, targetConsumption: DemographicRange, units: string,
+              sentiment: DemographicScaleSectorSentimentEnum) {
     this.title = title;
     this.consumption = Math.round(consumption * 100) / 100;
+    this.targetConsumption = new DemographicRange(Math.round(targetConsumption.start * 100) / 100,
+      Math.round(targetConsumption.end * 100) / 100);
     this.units = units;
+    this.sentiment = sentiment;
+    this.textClass = this.getTextClass(sentiment);
+    this.iconClass = this.getIconClass(sentiment);
   }
+
+  private getTextClass(sentiment: DemographicScaleSectorSentimentEnum): string {
+    if ([DemographicScaleSectorSentimentEnum.TOO_LOW,
+        DemographicScaleSectorSentimentEnum.LOW,
+        DemographicScaleSectorSentimentEnum.HIGH,
+        DemographicScaleSectorSentimentEnum.TOO_HIGH,].indexOf(sentiment) > -1) {
+      return "text-danger";
+    } else if ([DemographicScaleSectorSentimentEnum.BIT_LOW,
+        DemographicScaleSectorSentimentEnum.BIT_HIGH].indexOf(sentiment) > -1) {
+      return "text-warning";
+    } else {
+      return "text-success";
+    }
+  }
+
+  private getIconClass(sentiment: DemographicScaleSectorSentimentEnum): string {
+    let mp = new Map([
+      [DemographicScaleSectorSentimentEnum.TOO_LOW, "fa-angle-double-down"],
+      [DemographicScaleSectorSentimentEnum.LOW, "fa-angle-double-down"],
+      [DemographicScaleSectorSentimentEnum.BIT_LOW, "fa-angle-down"],
+      [DemographicScaleSectorSentimentEnum.GOOD, "fa-crosshairs"],
+      [DemographicScaleSectorSentimentEnum.EXCELLENT, "fa-crosshairs"],
+      [DemographicScaleSectorSentimentEnum.BIT_HIGH, "fa-angle-up"],
+      [DemographicScaleSectorSentimentEnum.HIGH, "fa-angle-double-up"],
+      [DemographicScaleSectorSentimentEnum.TOO_HIGH, "fa-angle-double-up"],
+    ]);
+    return mp.get(sentiment);
+  }
+
 }
