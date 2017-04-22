4a0ec29dcdfc1d143c89e9e1ac0a59d353dd6804
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-04-22 12:45:42 +0100

    merge

--- src/animations/animate.component.ts
+++ src/animations/animate.component.ts
@@ -19,189 +19,189 @@ import {AnimateActionEnum, AnimateActionAlias} from "./animate-action.enum";
       state(AnimateActionEnum.Visible.toString(), style({opacity: 1})),
       state(AnimateActionEnum.Hidden.toString(), style({opacity: 0})),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.FadeIn.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.FadeIn.toString(), [
         animate(ANIMATION_DURATION, keyframes([
           style({opacity: 0, offset: 0}),
           style({opacity: 1, offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.FadeInUp.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.FadeInUp.toString(), [
         animate(ANIMATION_DURATION, keyframes([
-          style({opacity: 0, transform: `translateY(${FADE_START_OFFSET})`, offset: 0}),
+          style({opacity: 0, transform: "translateY(" + FADE_START_OFFSET + ")", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateY(-${FADE_BOUNCE_OFFSET})`,
+            transform: "translateY(" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
           style({opacity: 1, transform: "translate(0,0)", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.FadeInUpBig.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.FadeInUpBig.toString(), [
         animate(ANIMATION_BIG_DURATION, keyframes([
-          style({opacity: 0, transform: `translateY(${FADE_BIG_START_OFFSET})`, offset: 0}),
+          style({opacity: 0, transform: "translateY(" + FADE_START_OFFSET + ")", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateY(-${FADE_BOUNCE_OFFSET})`,
+            transform: "translateY(-" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
           style({opacity: 1, transform: "translate(0,0)", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.FadeInDown.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.FadeInDown.toString(), [
         animate(ANIMATION_DURATION, keyframes([
-          style({opacity: 0, transform: `translateY(-${FADE_START_OFFSET})`, offset: 0}),
+          style({opacity: 0, transform: "translateY(-" + FADE_START_OFFSET + ")", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateY(${FADE_BOUNCE_OFFSET})`,
+            transform: "translateY(" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
           style({opacity: 1, transform: "translate(0,0)", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.FadeInDownBig.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.FadeInDownBig.toString(), [
         animate(ANIMATION_BIG_DURATION, keyframes([
-          style({opacity: 0, transform: `translateY(-${FADE_BIG_START_OFFSET})`, offset: 0}),
+          style({opacity: 0, transform: "translateY(-" + FADE_START_OFFSET + ")", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateY(${FADE_BOUNCE_OFFSET})`,
+            transform: "translateY(" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
           style({opacity: 1, transform: "translate(0,0)", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.FadeInLeft.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.FadeInLeft.toString(), [
         animate(ANIMATION_DURATION, keyframes([
-          style({opacity: 0, transform: `translateX(-${FADE_START_OFFSET})`, offset: 0}),
+          style({opacity: 0, transform: "translateX(-" + FADE_START_OFFSET + ")", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateX(${FADE_BOUNCE_OFFSET})`,
+            transform: "translateX(" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
           style({opacity: 1, transform: "translate(0,0)", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.FadeInLeftBig.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.FadeInLeftBig.toString(), [
         animate(ANIMATION_BIG_DURATION, keyframes([
-          style({opacity: 0, transform: `translateX(-${FADE_BIG_START_OFFSET})`, offset: 0}),
+          style({opacity: 0, transform: "translateX(-" + FADE_START_OFFSET + ")", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateX(${FADE_BOUNCE_OFFSET})`,
+            transform: "translateX(" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
           style({opacity: 1, transform: "translate(0,0)", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Visible.toString()} => ${AnimateActionEnum.FadeOutLeft.toString()}`, [
+      transition(AnimateActionEnum.Visible.toString() + "=>" + AnimateActionEnum.FadeOutLeft.toString(), [
         animate(ANIMATION_DURATION, keyframes([
           style({opacity: 1, transform: "translate(0,0)", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateX(${FADE_BOUNCE_OFFSET})`,
+            transform: "translateX(" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
-          style({opacity: 0, transform: `translateX(-${FADE_START_OFFSET})`, offset: 1.0})
+          style({opacity: 0, transform: "translateX(-" + FADE_START_OFFSET + ")", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Visible.toString()} => ${AnimateActionEnum.FadeOut.toString()}`, [
+      transition(AnimateActionEnum.Visible.toString() + "=>" + AnimateActionEnum.FadeOut.toString(), [
         animate(ANIMATION_DURATION, keyframes([
           style({opacity: 1, offset: 0}),
           style({opacity: 0, offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Visible.toString()} => ${AnimateActionEnum.FadeOutDown.toString()}`, [
+      transition(AnimateActionEnum.Visible.toString() + "=>" + AnimateActionEnum.FadeOutDown.toString(), [
         animate(ANIMATION_DURATION, keyframes([
           style({opacity: 1, transform: "translate(0,0)", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateY(-${FADE_BOUNCE_OFFSET})`,
+            transform: "translateY(-" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
-          style({opacity: 0, transform: `translateY(${FADE_START_OFFSET})`, offset: 1.0})
+          style({opacity: 0, transform: "translateY(" + FADE_START_OFFSET + ")", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Visible.toString()} => ${AnimateActionEnum.FadeOutDownBig.toString()}`, [
+      transition(AnimateActionEnum.Visible.toString() + "=>" + AnimateActionEnum.FadeOutDownBig.toString(), [
         animate(ANIMATION_BIG_DURATION, keyframes([
           style({opacity: 1, transform: "translate(0,0)", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateY(-${FADE_BOUNCE_OFFSET})`,
+            transform: "translateY(-" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
-          style({opacity: 0, transform: `translateY(${FADE_BIG_START_OFFSET})`, offset: 1.0})
+          style({opacity: 0, transform: "translateY(" + FADE_START_OFFSET + ")", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Visible.toString()} => ${AnimateActionEnum.FadeOutLeftBig.toString()}`, [
+      transition(AnimateActionEnum.Visible.toString() + "=>" + AnimateActionEnum.FadeOutLeftBig.toString(), [
         animate(ANIMATION_BIG_DURATION, keyframes([
           style({opacity: 1, transform: "translate(0,0)", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateX(${FADE_BOUNCE_OFFSET})`,
+            transform: "translateX(" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
-          style({opacity: 0, transform: `translateX(-${FADE_BIG_START_OFFSET})`, offset: 1.0})
+          style({opacity: 0, transform: "translateX(-" + FADE_START_OFFSET + ")", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.FadeInRight.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.FadeInRight.toString(), [
         animate(ANIMATION_DURATION, keyframes([
-          style({opacity: 0, transform: `translateX(${FADE_START_OFFSET})`, offset: 0}),
+          style({opacity: 0, transform: "translateX(" + FADE_START_OFFSET + ")", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateX(-${FADE_BOUNCE_OFFSET})`,
+            transform: "translateX(-" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
           style({opacity: 1, transform: "translate(0,0)", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.FadeInRightBig.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.FadeInRightBig.toString(), [
         animate(ANIMATION_BIG_DURATION, keyframes([
-          style({opacity: 0, transform: `translateX(${FADE_BIG_START_OFFSET})`, offset: 0}),
+          style({opacity: 0, transform: "translateX(" + FADE_START_OFFSET + ")", offset: 0}),
           style({
             opacity: 1,
-            transform: `translateX(-${FADE_BOUNCE_OFFSET})`,
+            transform: "translateX(-" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
           style({opacity: 1, transform: "translate(0,0)", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Visible.toString()} => ${AnimateActionEnum.FadeOutRight.toString()}`, [
+      transition(AnimateActionEnum.Visible.toString() + "=>" + AnimateActionEnum.FadeOutRight.toString(), [
         animate(ANIMATION_DURATION, keyframes([
           style({opacity: 1, transform: "translate(0,0)", offset: 0}),
           style({
             opacity: 1,
-            transform: `-translateX(${FADE_BOUNCE_OFFSET})`,
+            transform: "-translateX(" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
-          style({opacity: 0, transform: `translateX(${FADE_START_OFFSET})`, offset: 1.0})
+          style({opacity: 0, transform: "translateX(" + FADE_START_OFFSET + ")", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Visible.toString()} => ${AnimateActionEnum.FadeOutRightBig.toString()}`, [
+      transition(AnimateActionEnum.Visible.toString() + "=>" + AnimateActionEnum.FadeOutRightBig.toString(), [
         animate(ANIMATION_BIG_DURATION, keyframes([
           style({opacity: 1, transform: "translate(0,0)", offset: 0}),
           style({
             opacity: 1,
-            transform: `-translateX(${FADE_BOUNCE_OFFSET})`,
+            transform: "-translateX(" + FADE_START_OFFSET + ")",
             offset: FADE_BOUNCE_START_FRAME_OFFSET
           }),
-          style({opacity: 0, transform: `translateX(${FADE_BIG_START_OFFSET})`, offset: 1.0})
+          style({opacity: 0, transform: "translateX(" + FADE_START_OFFSET + ")", offset: 1.0})
         ]))
       ]),
 
-      transition(`${AnimateActionEnum.Hidden.toString()} => ${AnimateActionEnum.ZoomIn.toString()}`, [
+      transition(AnimateActionEnum.Hidden.toString() + "=>" + AnimateActionEnum.ZoomIn.toString(), [
         animate(ANIMATION_DURATION, keyframes([
           style({opacity: 0, transform: 'scale(0)'}),
           style({opacity: 1, transform: 'scale(1.1)'}),
