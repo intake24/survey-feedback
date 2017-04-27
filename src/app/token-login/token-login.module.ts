76cfacdfb152115052bb1e943aafc565cd160513
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-04-27 14:45:39 +0100

    token login loader

--- src/app/token-login/token-login.module.ts
+++ src/app/token-login/token-login.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TokenLoginComponent } from './token-login.component';
 import {TokenLoginRoutingModule} from "./token-login-routing.module";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    TokenLoginRoutingModule
+    TokenLoginRoutingModule,
+    SharedModule
   ],
   declarations: [TokenLoginComponent]
 })
