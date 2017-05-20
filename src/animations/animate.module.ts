852bf43f4c7cee0b14c954192f6982e9422c5d0d
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-05-20 12:01:42 +0100

    animate change

--- src/animations/animate.module.ts
+++ src/animations/animate.module.ts
@@ -1,12 +1,11 @@
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {AnimateComponent} from "./animate.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule, BrowserAnimationsModule
+    CommonModule, BrowserAnimationsModule
   ],
   declarations: [AnimateComponent],
   exports: [AnimateComponent]
