9bc25f99b2294f2224284c69301829977cdb5aa6
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2018-01-15 11:54:22 +0000

    token login delay

--- src/app/conf.ts
+++ src/app/conf.ts
@@ -7,7 +7,7 @@ export class AppConfig {
   }
 
   static readonly apiBaseUrl: string = environment.production ? `${AppConfig.windowRef.apiBaseUrl}/` : "http://api-test.intake24.co.uk/";
-  static readonly surveyId: string = environment.production ? AppConfig.windowRef.surveyId : "demo";
+  static readonly surveyId: string = environment.production ? AppConfig.windowRef.surveyId : "newcastle-can";
   // static readonly apiBaseUrl: string = "http://api-test.intake24.co.uk/";
   // static readonly surveyId: string = "newcastle-can";
   static readonly surveyPath: string = `/surveys/${AppConfig.surveyId}`;
