88ac66c7a169a72587398df1098518724634ec4e
Ivan Poliakov <ivpolyakov@gmail.com>
2019-11-20 18:11:21 +0000

    Don't use hardcoded API url

--- src/app/conf.ts
+++ src/app/conf.ts
@@ -6,10 +6,10 @@ export class AppConfig {
     return window;
   }
 
-  // static readonly apiBaseUrl: string = environment.production ? `${AppConfig.windowRef.apiBaseUrl}/` : 'http://localhost:9001/';
-  // static readonly surveyId: string = environment.production ? AppConfig.windowRef.surveyId : 'demo';
-  static readonly apiBaseUrl: string = 'https://api.intake24.co.uk/';
-  static readonly surveyId: string = 'demo';
+  static readonly apiBaseUrl: string = environment.production ? `${AppConfig.windowRef.apiBaseUrl}/` : 'http://localhost:9001/';
+  static readonly surveyId: string = environment.production ? AppConfig.windowRef.surveyId : 'demo';
+  //static readonly apiBaseUrl: string = 'https://api.intake24.co.uk/';
+  //static readonly surveyId: string = 'demo';
   static readonly surveyPath: string = `/surveys/${AppConfig.surveyId}`;
   static readonly privacyUrl: string = 'https://intake24.co.uk/info/privacy';
   static readonly termsUrl: string = 'https://intake24.co.uk/info/terms';
